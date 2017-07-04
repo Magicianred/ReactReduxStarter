a1b7378c10cb8de6542801026c228a8472ee0e2a
Marco Poletto <marco@shoot-the-moon.co.uk>
2017-07-04 17:20:39 +0100

    1-15

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAnn1i8IEIEh-PI7cbL3h-OlMjTHoM_Z0M';
 
 const App = () => {
-    return  <div>Hi!</div>;
+    return (  
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
